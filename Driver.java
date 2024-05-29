d21d45498be93944694b2fb0f1c7aa52e66ca4e3
Jayden Vuong <112903574+JaydenFloof@users.noreply.github.com>
2024-05-29 19:33:10 -0400

    temp modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,20 @@
 public class Driver {
-public static void main(String [] args) {
-Polynomial p = new Polynomial();
-System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
-Polynomial s = p1.add(p2);
-System.out.println("s(0.1) = " + s.evaluate(0.1));
-if(s.hasRoot(1))
-System.out.println("1 is a root of s");
-else
-System.out.println("1 is not a root of s");
-}
+    public static void main(String [] args) {
+
+        System.out.println("starting");
+        double [] c1 = {5, 2, 3};
+        int [] c2 = {1, 2, 3};
+        System.out.println("Defining p1\n");
+        Polynomial p1 = new Polynomial(c1, c2);
+
+        double [] a1 = {6, 4, 1};
+        int [] a2 = {1, 3, 4};
+        System.out.println("Defining p2\n");
+        Polynomial p2 = new Polynomial(a1, a2);
+        
+        System.out.println("Adding\n");
+        Polynomial s = p1.add(p2);
+
+        s.printy(s);
+    }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,33 +1,78 @@
 public class Polynomial {
 
     double[] coefficients;
+    int[] exponents;
 
     public Polynomial() {
         coefficients = new double[]{0};
+        exponents = new int[]{0};
     }
 
-    public Polynomial(double[] x) {
+    public Polynomial(double[] x, int[] y) {
         coefficients = new double[x.length];
+        exponents = new int[y.length];
         System.arraycopy(x, 0, coefficients, 0, x.length);
+        System.arraycopy(y, 0, exponents, 0, y.length);
     }
 
     public Polynomial add(Polynomial newPoly) {
-        int highest = Math.max(newPoly.coefficients.length, coefficients.length);
-        int lowest = Math.min(newPoly.coefficients.length, coefficients.length);
-        double[] result = new double[highest];
-        int k = 0;
-        for(int i=0; i<lowest; i++) {
-            result[i] = newPoly.coefficients[i] + coefficients[i];
-            k = i+1;
+        int highest_double = Math.max(newPoly.coefficients.length, coefficients.length);
+        // int lowest_double = Math.min(newPoly.coefficients.length, coefficients.length);
+        // int highest_int = Math.max(newPoly.exponents.length, exponents.length);
+        // int lowest_int = Math.min(newPoly.exponents.length, exponents.length);
+        int max_double = newPoly.coefficients.length + coefficients.length;
+        int max_int = newPoly.exponents.length + exponents.length;
+
+        double[] result_double = new double[max_double];
+        int[] result_int = new int[max_int];
+
+        // boolean check = false;
+        int gap = 0;
+
+        for(int i=0; i<highest_double; i++) {
+
+            if(newPoly.exponents[i] == exponents[i]) {
+                result_double[i+gap] = newPoly.coefficients[i] + coefficients[i];
+                result_int[i+gap] = exponents[i];
+                System.out.println("exponents for " + i + " are equal for " + exponents[i] + " and " + newPoly.exponents[i]);
+            }
+
+            else if(newPoly.exponents[i] < exponents[i]) {
+                System.out.println("exponents for " + i + " we have " + exponents[i] + " greater than " + newPoly.exponents[i]);
+
+                result_double[i+gap] = newPoly.coefficients[i];
+                result_int[i+gap] = newPoly.exponents[i];
+
+                result_double[i+1+gap] = coefficients[i];
+                result_int[i+1+gap] = exponents[i];
+
+                gap += 1;
+
+            }
+
+            else if(newPoly.exponents[i] > exponents[i]) {
+                System.out.println("exponents for " + i + " we have " + exponents[i] + " less than " + newPoly.exponents[i]);
+
+                result_double[i+gap] = coefficients[i];
+                result_int[i+gap] = exponents[i];
+
+                result_double[i+1+gap] = newPoly.coefficients[i];
+                result_int[i+1+gap] = newPoly.exponents[i];
+
+                gap += 1;
+            }
+            
         }
 
-        for(int j=k; j<highest; j++) {
-            if(newPoly.coefficients.length == highest) result[j] = newPoly.coefficients[j];
+        return new Polynomial(result_double, result_int);
+    }
 
-            else result[j] = coefficients[j];
+    public Polynomial printy(Polynomial poly) {
+        int max = Math.max(poly.coefficients.length, poly.exponents.length);
+        for(int i=0; i<max; i++){
+            System.out.println("For " + i + " the coef is " + poly.coefficients[i] + " the exp is " + poly.exponents[i]);
         }
-
-        return new Polynomial(result);
+        return poly;
     }
 
     public double evaluate(double x) {
